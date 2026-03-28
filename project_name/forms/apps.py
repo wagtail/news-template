ed5130c2faadfeeb8d17ffee1231be05a8b28fbb
ankit02327 <ankit2327@protonmail.com>
2026-03-28 14:47:25 +0530

    Fix incorrect AppConfig class name for forms app

--- project_name/forms/apps.py
+++ project_name/forms/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NewsConfig(AppConfig):
+class FormsConfig(AppConfig):
     default_auto_field: str = "django.db.models.AutoField"
     name = "{{ project_name }}.forms"
