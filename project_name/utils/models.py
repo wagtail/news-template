bcae54683b0830cedffd7eb161f9476683ca15ec
Aryan <aryanyk562@gmail.com>
2026-06-27 00:18:16 +0530

    Fixed Broken signup newsletter

--- project_name/utils/models.py
+++ project_name/utils/models.py
@@ -247,9 +247,9 @@ class SystemMessagesSettings(BaseSiteSetting):
         blank=True,
         max_length=255,
     )
-    footer_newsletter_signup_link = models.URLField(
+    footer_newsletter_signup_link = models.CharField(
         blank=True,
-        null=True,
+        max_length=255,
         help_text="Link to the newsletter signup form. If left blank, the signup CTA will not be displayed.",
     )
 
