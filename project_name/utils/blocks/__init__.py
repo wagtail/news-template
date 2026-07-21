f8aaa3ce4b804c4a41d35524920bec8ac1f1ca53
Aryan Khandhadiya <71654152+aryanyk@users.noreply.github.com>
2026-07-22 05:00:25 +0530

    Refactor block architecture into modular package (#145)
    * Refactor block architecture into modular package
    
    * Improved implementation of content.py

---
+++ project_name/utils/blocks/__init__.py
@@ -0,0 +1,45 @@
+from .content import (
+    AccordionBlock,
+    CardBlock,
+    CaptionedImageBlock,
+    FeaturedArticleBlock,
+    QuoteBlock,
+)
+from .links import (
+    ArticlePageLinkBlock,
+    ExternalLinkBlock,
+    InternalLinkBlock,
+    LinkStreamBlock,
+)
+from .layout import (
+    BaseCardSectionBlock,
+    BaseSectionBlock,
+    CTASectionBlock,
+    CardSectionBlock,
+    PlainCardSectionBlock,
+    SectionBlock,
+    SectionBlocks,
+    StatisticSectionBlock,
+    StoryBlock,
+)
+
+__all__ = [
+    "AccordionBlock",
+    "ArticlePageLinkBlock",
+    "BaseCardSectionBlock",
+    "BaseSectionBlock",
+    "CTASectionBlock",
+    "CaptionedImageBlock",
+    "CardBlock",
+    "CardSectionBlock",
+    "ExternalLinkBlock",
+    "FeaturedArticleBlock",
+    "InternalLinkBlock",
+    "LinkStreamBlock",
+    "PlainCardSectionBlock",
+    "QuoteBlock",
+    "SectionBlock",
+    "SectionBlocks",
+    "StatisticSectionBlock",
+    "StoryBlock",
+]
